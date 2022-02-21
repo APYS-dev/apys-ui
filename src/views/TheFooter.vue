1891935ffab6afd2d28b6f2945dfecd4c638af18
Vadim Shpet <vadik.shpet01@gmail.com>
2022-02-21 16:00:42 +0200

    added header&footer

---
+++ src/views/TheFooter.vue
@@ -0,0 +1,47 @@
+<template>
+  <footer>
+    <a href="#" target="_blank" rel="noopener noreferrer">
+      <img src="@/assets/img/social/twitter.png" alt="twitter" />
+    </a>
+    <a href="#" target="_blank" rel="noopener noreferrer">
+      <img src="@/assets/img/social/telegram.png" alt="telegram" />
+    </a>
+    <a href="#" target="_blank" rel="noopener noreferrer">
+      <img src="@/assets/img/social/medium.png" alt="medium" />
+    </a>
+    <a href="#" target="_blank" rel="noopener noreferrer">
+      <img src="@/assets/img/social/github.png" alt="github" />
+    </a>
+    <a href="#" target="_blank" rel="noopener noreferrer">
+      <img src="@/assets/img/social/doc.png" alt="doc" />
+    </a>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter',
+};
+</script>
+
+<style lang="scss" scoped>
+footer {
+  margin: 12px auto;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  gap: 16px;
+
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 38px;
+    height: 38px;
+  }
+
+  img {
+    width: 20px;
+  }
+}
+</style>
---
+++ src/views/TheHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <header>
+    <router-link to="/">
+      <img class="logo" src="@/assets/img/logo.svg" alt="Apys" />
+    </router-link>
+    <button v-if="isLogged" class="btn-big btn-border">logout</button>
+    <button v-else class="btn-border">Connect wallet</button>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+
+  data: () => ({
+    isLogged: true,
+  }),
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  margin: 16px 92px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .logo {
+    height: 26px;
+    width: auto;
+  }
+
+  @media screen and (max-width: 1600px) {
+    & {
+      margin: 16px auto;
+      padding: 0 24px;
+      max-width: 90%;
+    }
+  }
+}
+</style>
