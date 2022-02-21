6962475bb1efd8a9b69349c22897a1f6b9e37d9a
Vadim Shpet <vadik.shpet01@gmail.com>
2022-02-21 15:57:43 +0200

    catch unrecognized path

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
+import PageNotFound from '../views/PageNotFound.vue';
 
 const routes = [
   {
@@ -8,12 +9,8 @@ const routes = [
     component: Home,
   },
   {
-    path: '/components',
-    name: 'Components',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Components.vue'),
+    path: '/:catchAll(.*)', // Unrecognized path automatically matches 404
+    component: PageNotFound,
   },
 ];
 
