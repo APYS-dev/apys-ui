4d5c1f0119ca33d55d2460c8ed9e364330d40a55
Zeirison <zeirison@gmail.com>
2022-05-27 17:29:35 +0300

    Fix empty balances after deposit or withdraw transaction

--- src/store/modules/balances.js
+++ src/store/modules/balances.js
@@ -25,118 +25,122 @@ export default {
       context.commit('updateTokens', tokens);
     },
     async loadBalances(context, transactionMeta) {
-      // Get account balances
-      let apysBalances = {
-        balance: {},
-        withdraw: {},
-        deposit: {},
-      };
-      if (window.accountId) {
-        apysBalances = await view({
-          args: { account_id: window.accountId },
-          contractId: window.apysContractId,
-          methodName: 'get_balances',
-        });
+      // Return empty balances if no account
+      if (!window.accountId) {
+        const balances = context.state.tokens.map(async (token) => ({
+          token,
+          appBalance: '0',
+          walletBalance: '0',
+        }));
+
+        context.commit('updateBalances', balances);
+        return;
       }
 
-      const balances = context.state.tokens.map(async (token) => {
-        let wallet_balance = '0';
-        if (window.accountId) {
-          wallet_balance = await view({
+      // Get account balances
+      const apysBalances = await view({
+        args: { account_id: window.accountId },
+        contractId: window.apysContractId,
+        methodName: 'get_balances',
+      });
+
+      console.log('apysBalances', apysBalances);
+
+      const balances = await Promise.all(
+        context.state.tokens.map(async (token) => {
+          const wallet_balance = await view({
             args: { account_id: window.accountId },
             contractId: token.contractId,
             methodName: 'ft_balance_of',
           });
-        }
 
-        // Recalculate app balance using processing balances from APYS contract
-        const apysBalance = Big(apysBalances.balance[token.contractId] || 0);
-        const depositAmount = Big(apysBalances.deposit[token.contractId] || 0);
+          // Recalculate app balance using processing balances from APYS contract
+          const apysBalance = Big(apysBalances.balance[token.contractId] || 0);
+          const depositAmount = Big(apysBalances.deposit[token.contractId] || 0);
 
-        // Calculate and format app balance
-        let appBalance = fromUnits(apysBalance, token.decimals).toString();
+          console.log(`\n\n ${token.contractId} apysBalance: ${apysBalance}\n depositAmount: ${depositAmount}\n\n`);
 
-        // Get wallet balance
-        let walletBalance = fromUnits(wallet_balance, token.decimals).toString();
+          // Calculate and format app balance
+          let appBalance = fromUnits(apysBalance, token.decimals).toString();
 
-        // Check that balance is updated, if not, just update by transactionMeta
-        if (transactionMeta !== undefined && Object.keys(transactionMeta).length > 0) {
-          // Check deposit transaction
-          if (transactionMeta.deposit) {
-            if (transactionMeta.deposit[token.contractId] === undefined) {
-              return;
+          // Get wallet balance
+          let walletBalance = fromUnits(wallet_balance, token.decimals).toString();
+
+          // Check that balance is updated, if not, just update by transactionMeta
+          if (transactionMeta && Object.keys(transactionMeta).length > 0) {
+            // Check deposit transaction
+            if (transactionMeta.deposit && transactionMeta.deposit[token.contractId]) {
+              console.warn(
+                'TRIGGERED DEPOSIT',
+                `wallet: ${walletBalance} appBalance: ${appBalance}\n transactionMeta: ${JSON.stringify(
+                  transactionMeta
+                )}`
+              );
+
+              // Get transaction amounts
+              const amounts = transactionMeta.deposit[token.contractId];
+              if (walletBalance === amounts.oldWalletBalance) {
+                walletBalance = Big(walletBalance).minus(amounts.amount).toString();
+              }
+              if (appBalance === amounts.oldAppBalance) {
+                appBalance = Big(appBalance).plus(amounts.amount).toString();
+              }
             }
 
-            console.warn(
-              'TRIGGERED DEPOSIT',
-              `wallet: ${walletBalance} appBalance: ${appBalance}\n transactionMeta: ${JSON.stringify(transactionMeta)}`
-            );
+            // Check withdraw transaction
+            if (transactionMeta.withdraw && transactionMeta.withdraw[token.contractId]) {
+              console.warn(
+                'TRIGGERED WITHDRAW',
+                `wallet: ${walletBalance} appBalance: ${appBalance}\n transactionMeta: ${JSON.stringify(
+                  transactionMeta
+                )}`
+              );
 
-            // Get transaction amounts
-            const amounts = transactionMeta.deposit[token.contractId];
-            if (walletBalance === amounts.oldWalletBalance) {
-              walletBalance = Big(walletBalance).minus(amounts.amount).toString();
-            }
-            if (appBalance === amounts.oldAppBalance) {
-              appBalance = Big(appBalance).plus(amounts.amount).toString();
-            }
-          }
-
-          // Check withdraw transaction
-          if (transactionMeta.withdraw) {
-            if (transactionMeta.withdraw[token.contractId] === undefined) {
-              return;
+              // Get transaction amounts
+              const amounts = transactionMeta.withdraw[token.contractId];
+              if (walletBalance === amounts.oldWalletBalance) {
+                walletBalance = Big(walletBalance).plus(amounts.amount).toString();
+              }
+              if (appBalance === amounts.oldAppBalance) {
+                appBalance = Big(appBalance).minus(amounts.amount).toString();
+              }
             }
 
-            console.warn(
-              'TRIGGERED WITHDRAW',
-              `wallet: ${walletBalance} appBalance: ${appBalance}\n transactionMeta: ${JSON.stringify(transactionMeta)}`
-            );
+            // Check vault transaction
+            if (transactionMeta.vaultDeposit && transactionMeta.vaultDeposit[token.contractId]) {
+              console.warn(
+                'TRIGGERED VAULT DEPOSIT',
+                `wallet: ${walletBalance} appBalance: ${appBalance}\n transactionMeta: ${JSON.stringify(
+                  transactionMeta
+                )}`
+              );
 
-            // Get transaction amounts
-            const amounts = transactionMeta.withdraw[token.contractId];
-            if (walletBalance === amounts.oldWalletBalance) {
-              walletBalance = Big(walletBalance).plus(amounts.amount).toString();
-            }
-            if (appBalance === amounts.oldAppBalance) {
-              appBalance = Big(appBalance).minus(amounts.amount).toString();
-            }
-          }
-
-          // Check vault transaction
-          if (transactionMeta.vaultDeposit) {
-            if (transactionMeta.vaultDeposit[token.contractId] === undefined) {
-              return;
-            }
-
-            console.warn(
-              'TRIGGERED VAULT DEPOSIT',
-              `wallet: ${walletBalance} appBalance: ${appBalance}\n transactionMeta: ${JSON.stringify(transactionMeta)}`
-            );
-
-            // Get transaction amounts
-            const amounts = transactionMeta.vaultDeposit[token.contractId];
-            if (appBalance === amounts.oldAppBalance) {
-              appBalance = Big(appBalance).minus(amounts.amount).toString();
+              // Get transaction amounts
+              const amounts = transactionMeta.vaultDeposit[token.contractId];
+              if (appBalance === amounts.oldAppBalance) {
+                appBalance = Big(appBalance).minus(amounts.amount).toString();
+              }
             }
           }
-        }
 
-        // Print debug data
-        console.log('--------');
-        console.log('token', token.symbol);
-        console.log('balance', walletBalance);
-        console.log('apysBalance', appBalance);
-        console.log('depositAmount', depositAmount.toFixed(4));
-        console.log('--------');
-        return {
-          appBalance,
-          walletBalance,
-          token,
-        };
-      });
-      const res = (await Promise.all(balances)).filter((el) => !!el);
-      context.commit('updateBalances', res);
+          // Print debug data
+          console.log('--------');
+          console.log('token', token.symbol);
+          console.log('balance', walletBalance);
+          console.log('apysBalance', appBalance);
+          console.log('depositAmount', depositAmount.toFixed(4));
+          console.log('--------');
+          return {
+            appBalance,
+            walletBalance,
+            token,
+          };
+        })
+      );
+
+      console.log('valances', balances);
+
+      context.commit('updateBalances', balances);
     },
     async loadVaultsBalances(context, { strategies, transactionMeta }) {
       const vaultsUUIDs = strategies.map((it) => it.uuid);
