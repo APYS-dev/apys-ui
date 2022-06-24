6d853e4213545e229ab12f2a9360716a74447dec
Zeirison <zeirison@gmail.com>
2022-06-24 20:10:57 +0300

    Create formatters and constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,7 @@
+import Big from "big.js";
+
+// Transactions gas
+export const DEFAULT_GAS = Big("50000000000000");
+export const WITHDRAW_GAS = Big("100000000000000");
+export const STOP_STRATEGY_GAS = Big("200000000000000");
+export const ONE_YOCTO_NEAR = Big("1");
---
+++ src/utils/formatters.ts
@@ -0,0 +1,34 @@
+import Big from "big.js";
+
+export function formatPrice(
+  price?: number,
+  skipCurrencySign = false,
+  defaultValue = "n/a"
+): string {
+  if (!price || price === 0) {
+    return defaultValue;
+  }
+
+  const currencyFormat = new Intl.NumberFormat("en-US", {
+    useGrouping: true,
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 2,
+    notation: "standard",
+  });
+
+  const formattedValue = currencyFormat.format(price);
+
+  return (skipCurrencySign ? "" : "$") + formattedValue;
+}
+
+export function formatAmount(
+  amount: Big,
+  options: {
+    decimals: number;
+    fractionDigits: number;
+  }
+): string {
+  return amount
+    .div(new Big(10).pow(options.decimals))
+    .toFixed(options.fractionDigits);
+}
