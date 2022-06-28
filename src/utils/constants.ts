2b4f62fbf22b1689a67e3f32e8ef396695af2b1c
Zeirison <zeirison@gmail.com>
2022-06-28 16:55:28 +0300

    Add new constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,5 +3,7 @@ import Big from "big.js";
 // Transactions gas
 export const DEFAULT_GAS = Big("50000000000000");
 export const WITHDRAW_GAS = Big("100000000000000");
+export const DEPOSIT_TO_STRATEGY_GAS = Big("300000000000000");
 export const STOP_STRATEGY_GAS = Big("200000000000000");
 export const ONE_YOCTO_NEAR = Big("1");
+export const ZERO_YOCTO_NEAR = Big("0");
