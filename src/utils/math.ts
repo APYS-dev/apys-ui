49f553e6419d9306c974192af443e09250aa701e
Zeirison <zeirison@gmail.com>
2022-06-29 00:53:58 +0300

    Fix site performance

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,11 +1,7 @@
 import type Big from "big.js";
 
-export function aprToApy(apr: Big, periodInDays: number): Big {
-  return apr
-    .div(100)
-    .div(periodInDays)
-    .plus(1)
-    .pow(periodInDays)
-    .minus(1)
-    .mul(100);
+export function aprToApy(apr: Big, periodInDays: number): number {
+  return (
+    (Math.pow(apr.toNumber() / 100 / periodInDays + 1, periodInDays) - 1) * 100
+  );
 }
